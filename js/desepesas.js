5675db3292341f1924aea7408ff2d14d94b54fb7
LeviGomes <levidebritogomes@gmail.com>
2024-11-09 19:53:21 -0300

    Tela "Despesas" terminada

--- js/desepesas.js
+++ js/desepesas.js
@@ -1,3 +1,22 @@
+import {despesas} from './mock.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tbody = document.getElementById('tabela-despesas');
+
+    despesas.forEach(despesa => {
+        const row = document.createElement('tr');
+        
+        row.innerHTML = `
+            <td class="text-primary">${despesa.nome}</td>
+            <td>${despesa.data}</td>
+            <td>${despesa.contaBancaria}</td>
+            <td class="text-danger">${despesa.valor}</td>
+            <td class="text-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"><button class="btn-lixo"><i class="bi bi-three-dots"></i></button></td>
+        `;
+
+        tbody.appendChild(row);
+    });
+});
 
 
-document.querySelector()
+
---
+++ js/mock.js
@@ -0,0 +1,11 @@
+export const despesas = [
+    { nome: "Aluguel", data: "05/11/2024", contaBancaria: "Nubanck", valor: "R$2.000,00" },
+    { nome: "Luz", data: "05/11/2024", contaBancaria: "Nubanck", valor: "R$292,00" },
+    { nome: "Hospital", data: "04/11/2024", contaBancaria: "Itaú", valor: "R$140,42" },
+    { nome: "Mercado", data: "04/11/2024", contaBancaria: "Nubanck", valor: "R$653,00" },
+    { nome: "Internet", data: "04/11/2024", contaBancaria: "Bradesco", valor: "R$532,12" },
+    { nome: "Academia", data: "04/11/2024", contaBancaria: "Nubanck", valor: "R$130,00" },
+    { nome: "Mercado", data: "03/11/2024", contaBancaria: "Itaú", valor: "R$99,90" },
+    { nome: "Cartão de Crédito", data: "03/11/2024", contaBancaria: "Itaú", valor: "R$135,00" },
+    { nome: "Natação", data: "03/11/2024", contaBancaria: "Bradesco", valor: "R$70,00" }
+];
